2de209af9422fb06d8f34c92f4c85c0ce5057163
Pascal Kuthe <pascalkuthe@pm.me>
2023-10-28 02:11:59 +0200

    implement prefilter

--- src/literal.rs
+++ src/literal.rs
@@ -1,164 +1,132 @@
-use regex_automata::util::prefilter::Prefilter as Impl;
+pub use regex_automata::util::prefilter::Prefilter;
 pub use regex_automata::MatchKind;
 use regex_automata::Span;
 
+use crate::cursor::Cursor;
 use crate::Input;
 
 #[cfg(test)]
 mod tests;
 
-/// HACKS: we rely on implementation details of ropey to accelerate
-/// (the extremly common) case where the needle is as small as the
-/// minimum chunk width. Cessen will not like this but its just too
-/// tempting.
-///
-/// I think this is ok for the follwoing reasons:
-/// * there are essentially only two scenarios where this limit does not hold
-///    * the root node if its a leave: in that case there is only a single
-///      chunk, we only rely on this for multi chunks
-///    * If the text begins or ends with a CRLF pair (or multibyte char) that
-///      just pushed it over the edge in specific circumstances, then the leaf node
-///      that contains it must exceed `MAX_BYTES` to avoid splitting it.  This should be
-///      extremely rare and to account for it we substract 3 bytes form the actual limit
-///      to be save.
-/// * literal optimization speedup regex by (up to) an order of magnitude, but are basically not worth it if literal are spread across more than two chunks
-/// * the ropey version is pinned
-/// * I am fairly involved in ropey and would notice if this changed
-const ROPEY_MIN_CHUNK_LEN: usize = 29;
-
-#[derive(Clone, Debug)]
-pub struct Prefilter {
-    inner: Impl,
-    max_needle_len: usize,
+pub fn find<C: Cursor>(prefilter: &Prefilter, input: &mut Input<C>) -> Option<Span> {
+    if prefilter.max_needle_len() == 1 {
+        find_1(prefilter, input)
+    } else {
+        find_n(prefilter, input)
+    }
 }
 
-impl Prefilter {
-    pub fn new<B: AsRef<[u8]>>(kind: MatchKind, needles: &[B]) -> Option<Prefilter> {
-        Impl::new(kind, needles).and_then(|inner| {
-            let max_needle_len = needles
-                .iter()
-                .map(|needle| needle.as_ref().len())
-                .max()
-                .expect("atleast one needle required for construction prefilter");
-            // super long needles are super rare/mostly just not worth it. Just
-            // let the dfa handle that we are just prefiltering here after al
-            if max_needle_len > ROPEY_MIN_CHUNK_LEN {
-                return None;
+fn find_1<C: Cursor>(prefilter: &Prefilter, input: &mut Input<C>) -> Option<Span> {
+    debug_assert_eq!(prefilter.max_needle_len(), 1);
+    let first_haystack = &input.chunk();
+    if let Some(mut res) = prefilter
+        .find(first_haystack, Span { start: input.get_chunk_pos(), end: input.get_chunk_end() })
+    {
+        res.start += input.chunk_offset();
+        res.end += input.chunk_offset();
+        return Some(res);
+    }
+    while input.chunk_offset() + input.chunk().len() < input.end() && input.advance() {
+        let haystack = &input.chunk();
+        let Some(mut res) = prefilter.find(haystack, Span { start: 0, end: input.get_chunk_end() })
+        else {
+            continue;
+        };
+
+        res.start += input.chunk_offset();
+        res.end += input.chunk_offset();
+        return Some(res);
+    }
+    None
+}
+
+fn find_n<C: Cursor>(prefilter: &Prefilter, input: &mut Input<C>) -> Option<Span> {
+    let carry_over = prefilter.max_needle_len() - 1;
+    let sliding_window = 2 * carry_over;
+    let mut buf = Vec::with_capacity(3 * carry_over);
+    if let Some(mut res) = prefilter
+        .find(input.chunk(), Span { start: input.get_chunk_pos(), end: input.get_chunk_end() })
+    {
+        res.start += input.chunk_offset();
+        res.end += input.chunk_offset();
+        return Some(res);
+    }
+    #[cfg(debug_assertions)]
+    let mut found_large_chunk = false;
+
+    while input.chunk_offset() + input.chunk().len() < input.end() {
+        debug_assert!(
+            buf.is_empty()
+                || (buf.len() < sliding_window && (buf.len() >= carry_over || !found_large_chunk)),
+            "{} {} {}",
+            buf.len(),
+            carry_over,
+            found_large_chunk
+        );
+        let carry_over_start = input.chunk().len() - carry_over.min(input.chunk().len());
+        buf.extend_from_slice(&input.chunk()[carry_over_start..]);
+        if buf.len() >= sliding_window {
+            if let Some(mut res) = prefilter.find(&buf, Span { start: 0, end: buf.len() }) {
+                let buf_offset = input.chunk_offset() - buf.len();
+                res.start += buf_offset;
+                res.end += buf_offset;
+                return Some(res);
             }
-            Some(Prefilter { inner, max_needle_len })
-        })
-    }
-    pub fn wrap<B: AsRef<[u8]>>(imp: Option<Impl>, needles: &[B]) -> Option<Prefilter> {
-        imp.and_then(|inner| {
-            let max_needle_len = needles
-                .iter()
-                .map(|needle| needle.as_ref().len())
-                .max()
-                .expect("atleast one needle required for construction prefilter");
-            // super long needles are super rare/mostly just not worth it. Just
-            // let the dfa handle that we are just prefiltering here after al
-            if max_needle_len > ROPEY_MIN_CHUNK_LEN {
-                return None;
+            // rotate the end of the buffer
+            let shift_len = buf.len() - carry_over;
+            let (head, tail) = buf.split_at_mut(shift_len);
+            head[..carry_over].copy_from_slice(tail);
+            buf.truncate(carry_over);
+        }
+        if !input.advance() {
+            break;
+        }
+        if input.get_chunk_end() + buf.len() < sliding_window {
+            continue;
+        }
+
+        #[cfg(debug_assertions)]
+        {
+            found_large_chunk = true;
+        }
+
+        // assumption: its faster to copy and search on the contigous slice here
+        // TODO: verify with benchmarks
+        if input.get_chunk_end() + buf.len() < buf.capacity() {
+            let buf_offset = input.chunk_offset() - buf.len();
+            buf.extend_from_slice(input.chunk());
+            if let Some(mut res) = prefilter.find(&buf, Span { start: 0, end: buf.len() }) {
+                res.start += buf_offset;
+                res.end += buf_offset;
+                return Some(res);
             }
-            Some(Prefilter { inner, max_needle_len })
-        })
-    }
-
-    pub fn find(&self, input: &mut Input, span: Span) -> Option<Span> {
-        let old_span = input.get_span();
-        input.set_span(span);
-        let res = if self.max_needle_len == 1 { self.find_1(input) } else { self.find_n(input) };
-        input.set_span(old_span);
-        res
-    }
-
-    fn find_1(&self, input: &mut Input) -> Option<Span> {
-        debug_assert_eq!(self.max_needle_len, 1);
-        let start = input.move_to(input.start())?;
-        let first_haystack = input.haystack_fwd_truncated();
-        if let Some(mut res) =
-            self.inner.find(first_haystack, Span { start, end: first_haystack.len() })
-        {
-            res.start += input.haystack_off();
-            res.end += input.haystack_off();
-            return Some(res);
-        }
-        loop {
-            input.advance_fwd()?;
-            let haystack = input.haystack_fwd_truncated();
-            let Some(mut res) =
-                self.inner.find(haystack, Span { start: 0, end: first_haystack.len() })
-            else {
-                continue;
-            };
-
-            res.start += input.haystack_off();
-            res.end += input.haystack_off();
-            return Some(res);
-        }
-    }
-
-    fn find_n(&self, input: &mut Input) -> Option<Span> {
-        // ropey chunks are never smaller than 30
-        let carry_over = self.max_needle_len - 1;
-        debug_assert!(carry_over <= ROPEY_MIN_CHUNK_LEN);
-        let mut buf = Vec::with_capacity(self.max_needle_len * 2 - 2);
-        let start = input.move_to(input.start())?;
-        let first_haystack = input.haystack_fwd_truncated();
-        if let Some(mut res) =
-            self.inner.find(first_haystack, Span { start, end: first_haystack.len() })
-        {
-            res.start += input.haystack_off();
-            res.end += input.haystack_off();
-            return Some(res);
-        }
-        let prev_haystack_off = input.haystack_off();
-        input.advance_fwd()?;
-        let haystack = input.haystack_fwd_truncated();
-        let carry_over_start = first_haystack.len().saturating_sub(carry_over);
-        buf.extend_from_slice(&first_haystack[carry_over_start..]);
-        buf.extend_from_slice(&haystack[..carry_over.min(haystack.len())]);
-        if let Some(mut res) = self.inner.find(&buf, Span { start: 0, end: buf.len() }) {
-            res.start += prev_haystack_off + carry_over_start;
-            res.end += prev_haystack_off + carry_over_start;
-            return Some(res);
-        }
-        loop {
-            let haystack = input.haystack_fwd_truncated();
-            if let Some(mut res) = self.inner.find(haystack, Span { start: 0, end: haystack.len() })
+        } else {
+            let buf_offset = input.chunk_offset() - buf.len();
+            buf.extend_from_slice(&input.chunk()[..carry_over]);
+            if let Some(mut res) = prefilter.find(&buf, Span { start: 0, end: buf.len() }) {
+                res.start += buf_offset;
+                res.end += buf_offset;
+                return Some(res);
+            }
+            if let Some(mut res) =
+                prefilter.find(input.chunk(), Span { start: 0, end: input.get_chunk_end() })
             {
-                res.start += input.haystack_off();
-                res.end += input.haystack_off();
+                res.start += input.chunk_offset();
+                res.end += input.chunk_offset();
                 return Some(res);
             }
-
-            let prev_haystack = haystack;
-            let prev_haystack_off = input.haystack_off();
-            input.advance_fwd()?;
-            let carry_over_start = prev_haystack.len().saturating_sub(carry_over);
-            let haystack = input.haystack_fwd_truncated();
-            buf.extend_from_slice(&prev_haystack[prev_haystack.len().saturating_sub(carry_over)..]);
-            buf.extend_from_slice(&haystack[..carry_over.min(haystack.len())]);
-            if let Some(mut res) = self.inner.find(&buf, Span { start: 0, end: buf.len() }) {
-                res.start += prev_haystack_off + carry_over_start;
-                res.end += prev_haystack_off + carry_over_start;
-                return Some(res);
-            }
-            buf.clear();
+        }
+        buf.clear();
+    }
+    // this is a really weird edgecase where the total string is smaller than the
+    // longest neeldle len but larger than the shortest needel
+    if !buf.is_empty() {
+        if let Some(mut res) = prefilter.find(&buf, Span { start: 0, end: buf.len() }) {
+            let buf_offset = input.chunk_offset() - buf.len();
+            res.start += buf_offset;
+            res.end += buf_offset;
+            return Some(res);
         }
     }
-
-    // fn prefix(&self, haystack: &[u8], span: Span) -> Option<Span> {
-    //     self.inner.prefix(haystack, span)
-    // }
-
-    fn memory_usage(&self) -> usize {
-        self.inner.memory_usage()
-    }
-
-    // fn is_fast(&self) -> bool {
-    //     self.inner.is_fast()
-    // }
+    None
 }
-
-// fn search()
--- src/literal/tests.rs
+++ src/literal/tests.rs
@@ -1,27 +1,28 @@
 use std::iter::from_fn;
 
 use proptest::{prop_assert, proptest};
+use regex_automata::util::prefilter::Prefilter;
 use regex_automata::Span;
 
 proptest! {
     #[test]
-    fn matches(haystack: String, needle: [u8; super::ROPEY_MIN_CHUNK_LEN]) {
-        let foo = ropey::Rope::from_str(&haystack);
-        let Some(filter2) = regex_automata::util::prefilter::Prefilter::new(regex_automata::MatchKind::All, &[&needle]) else {
+    fn matches(mut haystack: String, needle: String) {
+        haystack = haystack.repeat(1024);
+        let needles = &[needle.as_bytes()];
+        let Some(prefilter) = Prefilter::new(regex_automata::MatchKind::All, needles) else {
             return Ok(())
         };
-        let filter1 = super::Prefilter::new(regex_automata::MatchKind::All, &[&needle]).unwrap();
-        let mut input = crate::Input::new(foo.slice(..));
-        let mut span = input.get_span();
+        let mut span = Span{ start: 0, end: haystack.len() };
         let iter1 = from_fn(||{
-            let res = filter1.find(&mut input, span)?;
+            let res = prefilter.find(haystack.as_bytes(), span)?;
             span.start = res.end;
             Some(res)
         });
-        let mut span = Span{start: 0, end: haystack.len()};
+        let rope = ropey::Rope::from_str(&haystack);
+        let mut input = crate::Input::new(rope.chunks());
         let iter2 = from_fn(||{
-            let res = filter2.find(haystack.as_bytes(), span)?;
-            span.start = res.end;
+            let res = super::find(&prefilter, &mut input)?;
+            input.set_range(res.end..);
             Some(res)
         });
         prop_assert!(iter1.eq(iter2));
